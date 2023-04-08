24ae18b2a10c5e3991b6045322c0ecbe8fc74f98
RahulShakya7 <rahulshakya225@gmail.com>
2023-04-08 14:15:03 +0545

    lab1 & lab2 completed

--- lab2/answer7.dart
+++ lab2/answer7.dart
@@ -1,11 +1,17 @@
 // Create a map with a name, and phone keys and store some values. Use where to find all keys that have length 4.
 void main(List<String> args) {
-  Map<String, dynamic> info = {
-    'name' : 'Rahul Shakya',
-    'phonekeys' : '45345',
+  Map<String, String> info = {
+    'Rahul': '9823476234',
+    'Dave': '9823434578',
+    'Carol': '9823454678',
+    'Chad': '9834345345',
+    'Avinav': '9885678978',
   };
-} 
+  length(info);
+}
 
-void length4(Map(String, dynamic)){
-  Iterable<String> length = names.where((name) => name.length(4));
+void length(Map<String, String> info) {
+  Iterable<String> length4 = info.keys.where((key) => key.length == 4);
+  print('All contacts: $info');
+  print('Keys with length 4: $length4');
 }
